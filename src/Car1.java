38f7bc0f1b5b7358b4d123d186c20c72d95e1ceb
kolabse <117442873+NatFedd@users.noreply.github.com>
2023-01-16 12:00:26 +0500

    16.01.2023

--- src/Car1.java
+++ src/Car1.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Car {
+public class Car1 {
+    private String brand;
+    private String model;
+    private float engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(float engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Car1{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
--- src/Car2.java
+++ src/Car2.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Car2 {
+public class Car2 {
+    private String brand;
+    private String model;
+    private float engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public Car2(String brand, String model, float engineVolume, String color, int year, String country) {
+          this.brand = brand;
+          this.model = model;
+          this.engineVolume = engineVolume;
+          this.color = color;
+          this.year = year;
+          this.country = country;
+      }
+          public String toString() {
+              return "Car2{" +
+                      "brand='" + brand + '\'' +
+                      ", model='" + model + '\'' +
+                      ", engineVolume=" + engineVolume +
+                      ", color='" + color + '\'' +
+                      ", year=" + year +
+                      ", country='" + country + '\'' +
+                      '}';
+    }
+
+
 }
